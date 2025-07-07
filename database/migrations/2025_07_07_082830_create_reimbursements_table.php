4d5bad9e54c67b2b19ed0372208097999e4735b7
Ghivarra Senandika Rushdie <gsenandika@gmail.com>
2025-07-07 16:13:59 +0700

    add number and make it nullable because the generation is using queue

--- database/migrations/2025_07_07_082830_create_reimbursements_table.php
+++ database/migrations/2025_07_07_082830_create_reimbursements_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create($this->tableName, function (Blueprint $table) {
             $table->uuid('id')->primary();
+            $table->string('number', 36)->unique()->nullable();
             $table->string('name', 255);
             $table->string('file', 255);
             $table->integer('amount', false, true)->default(0);
